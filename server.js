4636d88a32581b6a4ed5271ab8117e27ff4dfbec
Spencer Caldwell <spencer.caldwell@gmail.com>
2017-03-02 22:19:54 -0800

    changed listening port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+process.env.PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log('listening on port 3000');
 });
